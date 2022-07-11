7a4256e80101072e369733f73200713344654c8a
agasefamircan <agasefemircanov@gmail.com>
2022-07-12 01:18:20 +0400

    Initial commit

--- preorderTraversal.js
+++ preorderTraversal.js
@@ -1,3 +1,16 @@
-function preorderTraversal(root) {
-    
+var preorderTraversal = function(root) {
+    let arr=[];
+    traverse(root,arr);
+    return arr;
+};
+var traverse=function(root,arr){
+    if(root===null) {
+        return;
+    }
+    else
+    {
+    arr.push(root.val);
+    traverse(root.left,arr);
+    traverse(root.right,arr);
+    }
 };
