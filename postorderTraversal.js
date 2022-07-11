5b2549d151fb8307dce11790aae18e19334f7cfe
agasefamircan <agasefemircanov@gmail.com>
2022-07-12 01:08:39 +0400

    Initial commit

--- postorderTraversal.js
+++ postorderTraversal.js
@@ -1,3 +1,16 @@
-function postorderTraversal(root) {
-    
+var postorderTraversal = function(root) {
+    let arr=[];
+    traverse(root,arr);
+    return arr;
+};
+var traverse=function(root,arr){
+    if(root===null) {
+        return;
+    }
+    else
+    {
+    traverse(root.left,arr);
+    traverse(root.right,arr);
+    arr.push(root.val);
+    }
 };
